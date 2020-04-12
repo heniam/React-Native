dc7836214adf0f9297e3c31516e651da36469c1c
heniam <heniam2012@gmail.com>
2020-04-12 18:41:49 -0400

    Navigation Part 2
    Added Navigation Drawer

--- confusion/components/MainComponent.js
+++ confusion/components/MainComponent.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
-
-import { DISHES } from '../shared/dishes';
-
 
 const MenuNavigator = createStackNavigator(
   {
@@ -20,7 +19,7 @@ const MenuNavigator = createStackNavigator(
   },
   {
     initialRouteName: 'Menu',
-    defaultNavigationOptions: {
+    defaultNavigationOptions: ({navigation}) => ({
       headerStyle: {
         backgroundColor: '#512DA8',
       },
@@ -28,19 +27,63 @@ const MenuNavigator = createStackNavigator(
       headerTitleStyle: {
         color: '#fff',
       },
-    },
+    }),
   },
 );
 
-const MenuNavigatorContainer = createAppContainer(MenuNavigator);
+const HomeNavigator = createStackNavigator(
+  {
+    Home: {screen: Home},
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      headerStyle: {
+        backgroundColor: '#512DA8',
+      },
+      headerTitleStyle: {
+        color: '#fff',
+      },
+      headerTintColor: '#fff',
+    }),
+  },
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home',
+      },
+    },
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu',
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: '#D1C4E9',
+  },
+);
+
+const MainNavigatorContainer = createAppContainer(MainNavigator);
 
 
 class Main extends Component {
 
   render() {
     return (
-      <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight}}>
-        <MenuNavigatorContainer/>
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
+        }}>
+        <MainNavigatorContainer />
       </View>
     );
   }
